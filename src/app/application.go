a76680911628cb1e89d91d0c8541e0e570b6ecad
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-27 18:17:19 +0330

    added cassandra db

--- src/app/application.go
+++ src/app/application.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/Delaram-Gholampoor-Sagha/bookstore_oauth_api/src/clients/cassandra"
 	"github.com/Delaram-Gholampoor-Sagha/bookstore_oauth_api/src/domain/access_token"
 	"github.com/Delaram-Gholampoor-Sagha/bookstore_oauth_api/src/http"
 	"github.com/Delaram-Gholampoor-Sagha/bookstore_oauth_api/src/repository/db"
@@ -12,6 +13,11 @@ var (
 )
 
 func StartApplication() {
+	session, dbErr := cassandra.GetSession()
+	if dbErr != nil {
+		panic(dbErr)
+	}
+	session.Close()
 
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
 
--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,12 +1,13 @@
 package db
 
 import (
+	"github.com/Delaram-Gholampoor-Sagha/bookstore_oauth_api/src/clients/cassandra"
 	"github.com/Delaram-Gholampoor-Sagha/bookstore_oauth_api/src/domain/access_token"
 	"github.com/Delaram-Gholampoor-Sagha/bookstore_oauth_api/src/utils/errors"
 )
 
 func NewRepository() DBRepository {
-	return &dbRepository{} 
+	return &dbRepository{}
 }
 
 type dbRepository struct {
@@ -17,5 +18,10 @@ type DBRepository interface {
 }
 
 func (r *dbRepository) GetById(string) (*access_token.AccessToken, *errors.RestErr) {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
 	return nil, errors.NewIntervalServerError("database connection not implemented yet")
 }
