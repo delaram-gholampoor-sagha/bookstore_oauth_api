445f8368df0c85aa3dae640fd2cc85f6bd5f7587
Delaram Gholampoor Sagha <majesticdelaram@yah00.com>
2021-12-27 19:41:19 +0330

    create function based on the given access token

--- src/http/http_access_token.go
+++ src/http/http_access_token.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/Delaram-Gholampoor-Sagha/bookstore_oauth_api/src/domain/access_token"
+	"github.com/Delaram-Gholampoor-Sagha/bookstore_oauth_api/src/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
 type AccessTokenHandler interface {
 	GetById(c *gin.Context)
+	Create(c *gin.Context)
 }
 
 type accessTokenHandler struct {
@@ -32,5 +34,25 @@ func (handler *accessTokenHandler) GetById(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, accessToken)
+
+}
+
+// creating an access token based on on giving an access token
+// so far we have created an access token base on the existance of the user in the database
+func (handler *accessTokenHandler) Create(c *gin.Context) {
+	var at access_token.AccessToken
+	// this function takes the json request and attempts to use that request fill this access token based on this json configuration
+	if err := c.ShouldBindJSON(&at); err != nil {
+		restErr := errors.NewBadRequessrError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	if err := handler.service.Create(at); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, at)
 
 }
